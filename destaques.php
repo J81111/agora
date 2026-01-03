889500b0abcf44613f45fe6afb54c536d2b387f6
J81111 <maresia653@gmail.com>
2026-01-03 19:55:38 +0000

    corrige caminho do troco premiado

--- destaques.php
+++ destaques.php
@@ -108,7 +108,7 @@ $scratch_cards = [
     'url_view' => '?view=centavo-da-sorte'
 ],
 
-    ['img'=>'https://ik.imagekit.io/3kbnnws8u/Tech%20Mania.png?updatedAt=1757350104698','alt'=>'Tech Mania','title'=>'Tech Mania','prize_info'=>'PRÊMIOS DE ATÉ R$ 2.500,00','price'=>'1,00', 'url_view' => 'sorte-instantanea'],
+    [img' => 'IMG/trocopremiado.png','alt'=>'Tech Mania','title'=>'Tech Mania','prize_info'=>'PRÊMIOS DE ATÉ R$ 2.500,00','price'=>'1,00', 'url_view' => 'sorte-instantanea'],
 
     ['img'=>'https://ik.imagekit.io/3kbnnws8u/apple%20mania.png?updatedAt=1757350104484','alt'=>'Apple Mania','title'=>'Apple Mania','prize_info'=>'PRÊMIOS DE ATÉ R$ 5.000,00','price'=>'2,50', 'url_view' => 'raspadinha-suprema'],
 
